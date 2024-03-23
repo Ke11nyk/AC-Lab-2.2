717f6499feba1ac6ba23e0a97ccfcc6f8561adbb
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-03-23 12:11:52 +0200

    Update Johnson Algorithm.cpp

--- Johnson Algorithm/Johnson Algorithm.cpp
+++ Johnson Algorithm/Johnson Algorithm.cpp
@@ -10,44 +10,25 @@ using namespace std;
 
 struct Edge
 {
-    int from;   // початкова вершина
-    int to;     // кінцева вершина
-    int weight;  // вага ребра
+    int from;   // start vertix
+    int to;     // final vertix
+    int weight;  // weight of edge
 };
 
 class Graph
 {
-public:
-    int V;  // кількість вершин
-    vector <Edge> edges;  // вектор ребер
+private:
+    int V;  // amount of vertices
+    vector <Edge> edges;  // vector of edges
 
-    vector<vector<pair<int, int>>> adj_list;  // список суміжності графа
+    vector<vector<pair<int, int>>> adj_list;  // graph adjacency list
 
-    Graph(vector<Edge>& edges, int V) : edges(edges), V(V)
-    {
-        adj_list.resize(V + 1);
-        for (const auto& edge : edges)
-            adj_list[edge.from].emplace_back(edge.to, edge.weight);
-    }
-
-    void print_graph()
-    {
-        for (int i = 1; i <= V; ++i) {
-            cout << "Vertice " << i << ": ";
-            for (const auto& neighbor : adj_list[i]) {
-                cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    vector <int> bellmanFord(int& V, vector <Edge> edges)
+    vector <int> BellmanFord(int& V, vector <Edge> edges)
     {
         /*
             Step 1 - Add a new vertex 0 to the given graph, add edges of
             weight 0 from the new vertex to all the existing vertices.
-            edge = {0, u, 0} (1 <= u <= N)
+            edge = {0, u, 0} (1 <= u <= V)
         */
         for (int i = 1; i <= V; i++)
             edges.push_back({ 0, i, 0 });
@@ -62,8 +43,8 @@ public:
         for (int i = 1; i < V; i++)
             for (auto e : edges)
                 if (dist[e.from] != LLONG_MAX && dist[e.to] > dist[e.from] + e.weight) dist[e.to] = dist[e.from] + e.weight;
-        
-        //The shortest distance values are values of h[].
+
+        // the shortest distance values are values of h[]
         return dist;
     }
 
@@ -73,15 +54,11 @@ public:
         vector<int> dist(V + 1, numeric_limits<int>::max());
         dist[src] = 0;
 
-        // Ініціалізуємо піраміду Фібоначчі
+        // initialize the Fibonacci heap
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({ 0, src });
 
-        // Initialize the parent array to track the shortest path
-        vector<vector<int>> p(V + 1);
-        //p[src] = src;
-
-        // Initialize the parent array to track the shortest path
+        // initialize the parent array to track the shortest path
         vector<int> parent(V + 1, -1);
         parent[src] = src;
 
@@ -97,7 +74,7 @@ public:
                 if (dist[f] + w < dist[s])
                 {
                     dist[s] = dist[f] + w;
-                    parent[s] = f;  // Update the parent vertex
+                    parent[s] = f;  // update the parent vertex
                     pq.push({ dist[s], s });
                 }
             }
@@ -108,15 +85,36 @@ public:
         return dist;
     }
 
+public:
+
+    Graph(vector<Edge>& edges, int V) : edges(edges), V(V)
+    {
+        adj_list.resize(V + 1);
+        for (const auto& edge : edges)
+            adj_list[edge.from].emplace_back(edge.to, edge.weight);
+    }
+
+    void print_graph()
+    {
+        for (int i = 1; i <= V; ++i) {
+            cout << "Vertex " << i << ": ";
+            for (const auto& neighbor : adj_list[i]) {
+                cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
     vector <vector<int>> Johnson(int E, vector<vector<int>>& pathes)
     {
-        // The shortest distance values are values of h[].
-        vector <int> h = bellmanFord(V, edges);
+        // the shortest distance values are values of h[]
+        vector <int> h = BellmanFord(V, edges);
 
         for (const Edge& edge : edges) {
             if (h[edge.from] != LLONG_MAX && h[edge.to] > h[edge.from] + edge.weight) {
                 cout << "The graph contains a cycle with negative weight." << endl;
-                return vector <vector<int>>(); // Повертаємо порожній вектор
+                return vector <vector<int>>(); // return empty vector
             }
         }
 
@@ -124,14 +122,15 @@ public:
         {
             int u = edges[i].from;
             int v = edges[i].to;
-            //For every pair of vertices u and v, having an edge (u -> v), add h[u] - h[v] to the edge weight.
+            // for every pair of vertices u and v, having an edge (u -> v), add h[u] - h[v] to the edge weight.
             edges[i].weight += (h[u] - h[v]);
         }
+
         /*
             2D matrix to store all-pairs shortest path
             path[u][v] = shortest path from u to v
         */
-        vector <vector<int>> path(V + 1, vector <int>(V + 1, INT_MAX));
+        vector <vector<int>> path(V + 1, vector <int>(V + 1, LLONG_MAX));
 
 
         /*
@@ -145,40 +144,40 @@ public:
 };
 
 
-
-
-
 signed main() {
-    //vertices, edges
+    // vertices, edges
     int V, E, from, to, weight, src;
     cout << "Enter the number of vertices and edges: ";
     cin >> V >> E;
+
     /*
         vector to store edges
-        edge - (u->v)
-        weight - w
-        edges.push_back({u, v, w})
+        edge - (from->to)
+        weight - weight
+        edges.push_back({from, to, weight})
     */
     vector<Edge> edges;
     cout << "Enter the edges in 'from to weight' format:\n";
     for (int i = 0; i < E; ++i)
     {
         cin >> from >> to >> weight;
-        edges.push_back({ from, to, weight });
+        edges.push_back({from, to, weight});
     }
-
-    vector<vector<int>> pathes(V + 1);
 
     Graph g(edges, V);
 
     g.print_graph();
 
-    // Виклик алгоритму Джонсона
+    // shortest paths
+    vector<vector<int>> pathes(V + 1);
+
+    // weight of shortest paths
     vector<vector<int>> all_distances = g.Johnson(E, pathes);
 
+    // if graph has negative cycle
     if (all_distances == vector<vector<int>>()) return 0;
 
-    // Дозволяє користувачу обрати вершину та вивести результат
+    // user can choose vertex for which result would be displayed
     while (true)
     {
         cout << "Enter the vertex number to display the shortest paths: ";
@@ -193,16 +192,15 @@ signed main() {
                 else
                 {
                     cout << src << " <-> " << i << " weight: " << all_distances[src][i] << " path: ";
-                    // Print the shortest path vertices
+                    // print the shortest path vertices
                     stack <int> path;
                     int v = i;
                     while (v != src)
                     {
-                        if (pathes[src][v] == -1) break;
                         path.push(v);
                         v = pathes[src][v];
                     }
-                    if (pathes[src][v] != -1) path.push(src);
+                    path.push(src);
 
                     while (!path.empty())
                     {
@@ -214,6 +212,7 @@ signed main() {
                 }
             }
         }
+        cout << endl;
     }
 
     return 0;
